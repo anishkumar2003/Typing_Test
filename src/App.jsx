60bccb5a4de33df804a5273c9d7ee89c5e21cda3
Anish Kumar <kumaranish200a@gmail.com>
2024-03-18 21:02:37 +0530

    Project Setup done

---
+++ src/App.jsx
@@ -0,0 +1,9 @@
+export default function App()
+{
+  return(
+    <>
+    <h1 className="text-orange-500">Matre class</h1>
+    <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Iusto adipisci tempora sint veniam numquam cupiditate perspiciatis modi, amet officiis! Iure quasi eligendi corporis modi dolor commodi maxime. Saepe, nisi incidunt!</p>
+    </>
+  )
+}
